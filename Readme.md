bc16d4762924e429dbf6038c277d95ddbc376e0b
Tommy Parnell <tparnell8@gmail.com>
2015-11-07 09:59:14 -0500

    commit the job

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## What is this?
+
+This is an azure webjob to run on  the schedule at the beginning of the month to update my [resume](https://github.com/tparnell8/Resume).
+
+Resume cli calculates everything at build time and so relative dates get out of wack as time passes.
+
---
+++ src/UpdateResumeData/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateResumeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(@"D:\home\site\wwwroot");
+            var info = new ProcessStartInfo("cmd.exe", @" /c node .\\node_modules\\resume-cli\\index.js export index -f html --theme elegant") { UseShellExecute = false };
+            var process = Process.Start(info);
+            process.WaitForExit();
+        }
+    }
+}
